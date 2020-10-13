a3d5591b6039289347c1a4eaf3e4a9275a7a49d6
danielsdesk <me@danielsdesk.com>
2020-10-13 07:44:08 +0000

    idk what is going on with test 14 but whatever

--- cypress/integration/14-testing-emails/start.js
+++ cypress/integration/14-testing-emails/start.js
@@ -12,11 +12,11 @@ it('sends a welcome email', () => {
 
   cy
     .get('[type=\'email\']')
-    .type('');
+    .type('distance-damage.mzt3fjhb@mailosaur.io');
 
   cy
     .get('[type=\'password\']')
-    .type('');
+    .type('admin');
 
   cy
     .contains('Send me a welcome email')
@@ -27,4 +27,19 @@ it('sends a welcome email', () => {
     .get('.signup-button')
     .click();
   
+  cy
+    .request({
+      method: 'POST',
+      url: 'https://mailosaur.com/api/messages/await?server=mzt3fjhb',
+      headers: {
+        authorization: 'Basic ' + Buffer.from('7e8nIuq7P8cVdAD').toString('base64')
+      },
+      body: {
+        sentTo: 'distance-damage.mzt3fjhb@mailosaur.io'
+      }
+    }).then( message => {
+
+      expect(message.body.html.links[0].href).to.eq('https://www.youtube.com/channel/UCDOCAVIhSh5VpJMEfdak1OA');
+
+    });
 });
