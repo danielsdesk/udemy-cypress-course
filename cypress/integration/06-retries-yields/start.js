6601c63ec9eb4e767c7eeb375b3e3985434969dd
danielsdesk <me@danielsdesk.com>
2020-10-13 06:33:52 +0000

    test 06

--- cypress/integration/06-retries-yields/start.js
+++ cypress/integration/06-retries-yields/start.js
@@ -7,25 +7,29 @@ beforeEach( () => {
 
 });
 
-it.only('Should have todo item with text "create a list of todos"', () => {
+it('Should have todo item with text "create a list of todos"', () => {
 
-  cy
-    .get('.todo')
-    .should('be.visible');
-  
+  cy.get('.new-todo')
+    .type('create a list of todos{enter}');
+
+  cy.get('.todo')
+    .should('be.visible')
+    .contains('todos');
 });
 
 it('Should have todo item with text "buy milk"', () => {
 
   cy
-    .get('.todo-list');
-    
+    .get('.todo-list')
+    .find('li')
+    .eq(1)
+    .contains('buy milk');
 });
 
-it('Should have one todo item', () => {
+it.only('Should have one todo item', () => {
 
   cy
-    .get('.todo')
+    .get('.todo', {timeout: 30000})
     .should('have.length', 1);
   
 });
