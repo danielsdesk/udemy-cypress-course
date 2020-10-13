9ab0bfaccdc2db6ad1c154bcc58f717a96a23b23
danielsdesk <me@danielsdesk.com>
2020-10-13 06:22:50 +0000

    test 03

--- cypress/integration/03-interact-with-elements/start.js
+++ cypress/integration/03-interact-with-elements/start.js
@@ -1,13 +1,19 @@
 /// <reference types="cypress" />
 
 it('adds a new todo item', () => {
-  
+    cy.visit('localhost:3000');
+    cy.get('.new-todo')
+    .type('buy milk{enter}');
 });
 
 it('completes todo item', () => {
-  
+    cy.visit('localhost:3000');
+    cy.get('.toggle')
+    .click();
 });
 
 it('deletes todo item', () => {
-  
+    cy.visit('localhost:3000');
+    cy.get('.destroy')
+    .click();
 });
