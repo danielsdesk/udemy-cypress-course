fdc15ae75ceef857cb6b8a3a861e207b092ea629
danielsdesk <me@danielsdesk.com>
2020-10-13 06:29:56 +0000

    test 05

--- cypress/integration/05-make-assertions/start.js
+++ cypress/integration/05-make-assertions/start.js
@@ -8,14 +8,32 @@ beforeEach( () => {
 
 });
 
-it.only('Adds one item', () => {
+it('Adds one item', () => {
+    cy.get('.new-todo')
+    .type('buy milk{enter}');
 
+    cy.get('.todo')
+    .should('be.visible');
 });
 
 it('Adds two items', () => {
-  
+    cy.get('.new-todo')
+    .type('buy milk{enter}');
+
+    cy.get('.new-todo')
+    .type('wash dishes{enter}');
+
+    cy.get('.todo')
+    .should('have.length', 2);
 });
 
-it('Marks item as completed', () => {
+it.only('Marks item as completed', () => {
+    cy.get('.new-todo')
+    .type('buy milk{enter}');
 
+    cy.get('.toggle')
+    .click();
+
+    cy.get('.todo')
+    .should('have.class', 'completed');
 });
